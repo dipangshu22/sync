60568a57f9e3da22878f70d6a9a8e9c100b4f2da
dipangshu22 <dipangshu22@gmail.com>
2025-07-12 14:46:44 +0530

    second

---
+++ nextcall.js
@@ -0,0 +1,15 @@
+function getdata(data,nextdata){
+setTimeout(()=>{
+
+    console.log("data:",data);
+    if(nextdata){
+        nextdata()
+    }
+},2000)
+
+}
+
+getdata(1 ,()=>{
+    getdata(22)
+
+})
---
+++ time.js
@@ -0,0 +1,8 @@
+function getdata(data){
+    console.log("data:",data)
+}
+
+setTimeout(() => {
+    console.log(getdata(4433))
+    
+},2000);
